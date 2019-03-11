3423e1ece7f1616df1768a2a8dd0bc956901efb6
viprachit <viprachit@gmail.com>
2019-03-11 11:57:22 +0530

    Add files via upload
    March 8th 2019

---
+++ pub age entry.py
@@ -0,0 +1,9 @@
+print("what is your age?")
+
+a = int(input("I am "))
+if a <= 17:
+    print("you are too young - no entry")
+elif a >= 18 and a <= 20:
+            print ("you are allowed to enter but No alcohol")
+else:
+    print("you can drink")
